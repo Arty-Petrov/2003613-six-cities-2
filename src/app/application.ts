65c09c1f9b18bb910600a7aab4ebccc2f0083dcb
Artyom Petrov <artyom.petrov@gmail.com>
2022-10-18 21:17:52 +0300

    Add Express.js to Application class

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,18 +1,23 @@
-import {inject, injectable} from 'inversify';
+import express, { Express } from 'express';
+import { inject, injectable } from 'inversify';
 import 'reflect-metadata';
-import {ConfigInterface} from '../common/config/config.interface.js';
-import {DatabaseInterface} from '../common/database-client/database.interface.js';
-import {LoggerInterface} from '../common/logger/logger.interface.js';
-import {Component} from '../types/component.types.js';
-import {getURI} from '../utils/db.js';
+import { ConfigInterface } from '../common/config/config.interface.js';
+import { DatabaseInterface } from '../common/database-client/database.interface.js';
+import { LoggerInterface } from '../common/logger/logger.interface.js';
+import { Component } from '../types/component.types.js';
+import { getURI } from '../utils/db.js';
 
 @injectable()
 export default class Application {
+  private expressApp: Express;
+
   constructor(
     @inject(Component.LoggerInterface) private logger: LoggerInterface,
     @inject(Component.ConfigInterface) private config: ConfigInterface,
     @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface
-  ) {}
+  ) {
+    this.expressApp = express();
+  }
 
   public async init() {
     this.logger.info('Application initialization…');
@@ -27,5 +32,8 @@ export default class Application {
     );
 
     await this.databaseClient.connect(uri);
+
+    this.expressApp.listen(this.config.get('PORT'));
+    this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
   }
 }
